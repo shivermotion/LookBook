046dd81c76c960d8e3c4c811e8d98fe00a5a350f
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2022-01-06 13:02:07 -0600

    pushing data from routes to handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,33 +5,29 @@ const { Book, User, Read } = require('../models');
 router.get('/dashboard', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
-    const bookData = await Book.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['username'], as: 'read_books'
-        },
-      ],
-    });
-
-    // const userData = await User.findByPk(req.session.user_id, {
-    //     attributes: { exclude: ['password'] },
-    //     include: [{ model: Read }],
-    //   });
+    const bookData = await Book.findAll();
+    const userData = await User.findAll();
     
+    const logged_in_userData = await User.findByPk(2, {
+        attributes: { exclude: ['password'] },
+        include: [
+          { 
+            model: Book,
+            attributes: ['id', 'title', 'author'], as: 'user_books' 
+          }
+        ],
+      });
 
-    res.json(bookData);
-    // res.json(userData);
-
-    // // Serialize data so the template can read it
-    // const books = bookData.map((book) => book.get({ plain: true }));
-    // const logged_in_user = userData.get({ plain: true });
-
+    // Serialize data so the template can read it
+    const books = bookData.map((book) => book.get({ plain: true }));
+    const users = userData.map((user) => user.get({ plain: true }));
+    const logged_in_user = logged_in_userData.get({ plain: true });
+console.log(logged_in_user);
     // // Pass serialized data and session flag into template
-    // res.render('homepage', { 
-    //   books, users, logged_in_user,
-    //   logged_in: req.session.logged_in 
-    // });
+    res.render('dashboard', { 
+      books, users, logged_in_user,
+      logged_in: req.session.logged_in 
+    });
   } catch (err) {
       console.log(err)
     res.status(500).json(err);
@@ -40,7 +36,10 @@ router.get('/dashboard', async (req, res) => {
 
 router.get('/book/:id', async (req, res) => {
   try {
-    const bookData = await Book.findByPk(req.params.id, {
+    const bookData = await Book.findAll();
+    const userData = await User.findAll();
+    
+    const currentBookData = await Book.findByPk(req.params.id, {
       include: [
         {
           model: Read,
@@ -49,14 +48,15 @@ router.get('/book/:id', async (req, res) => {
       ],
     });
 
-    res.json(bookData)
 
-    // const book = bookData.get({ plain: true });
+    const books = bookData.map((book) => book.get({ plain: true }));
+    const users = userData.map((user) => user.get({ plain: true }));
+    const currentBook = currentBookData.get({ plain: true });
 
-    // res.render('book', {
-    //   ...book,
-    //   logged_in: req.session.logged_in
-    // });
+    res.render('book', { 
+      books, users, currentBook,
+      logged_in: req.session.logged_in 
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -65,48 +65,34 @@ router.get('/book/:id', async (req, res) => {
 
 router.get('/user/:id', async (req, res) => {
     try {
-      const userData = await User.findByPk(req.params.id, {
+    const bookData = await Book.findAll();
+    const userData = await User.findAll();
+    
+    const currentUserData = await User.findByPk(2, {
+        attributes: { exclude: ['password'] },
         include: [
-          {
-            model: Read,
-            attributes: ['rating'],
-          },
+          { 
+            model: Book,
+            attributes: ['id', 'title', 'author'], as: 'user_books' 
+          }
         ],
       });
-  
-      res.json(userData)
-  
-      // const user = userData.get({ plain: true });
-  
-      // res.render('user', {
-      //   ...user,
-      //   logged_in: req.session.logged_in
-      // });
+    
+  // Serialize data so the template can read it
+    const books = bookData.map((book) => book.get({ plain: true }));
+    const users = userData.map((user) => user.get({ plain: true }));
+    const currentUser = currentUserData.get({ plain: true });
+console.log(currentUser);
+    // // Pass serialized data and session flag into template
+    res.render('user', { 
+      books, users, currentUser,
+      logged_in: req.session.logged_in 
+    });
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   });
-
-// Use withAuth middleware to prevent access to route
-// router.get('/dashboard', withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Project }],
-//     });
-
-//     const user = userData.get({ plain: true });
-
-//     res.render('dashboard', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 router.get('/', (req, res) => {
   // If the user is already logged in, redirect the request to another route
